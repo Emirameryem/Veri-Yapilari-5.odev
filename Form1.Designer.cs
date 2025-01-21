163e99c03767560cfbb0a94da1146331446ded59
Emira Meryem <158612061+Emirameryem@users.noreply.github.com>
2025-01-21 22:29:16 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace VeriYapilariOdev_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Posfixe dönüştür";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(354, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Prefixe dönüştür";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VeriYapilariOdev_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private string PostfixDonusumu(string infix)
+        {
+
+            Stack<char> yıgın = new Stack<char>(); 
+            string cıktı = ""; 
+            foreach (char a in infix)
+            {
+                if (char.IsLetterOrDigit(a)) 
+                
+                {
+                    cıktı += a; 
+                }
+                else if (a == '(') 
+                {
+                    yıgın.Push(a); 
+                }
+                else if (a == ')') 
+                {
+                    while (yıgın.Count > 0 && yıgın.Peek() != '(')
+                    {
+                        cıktı += yıgın.Pop(); // Yığından eleman al
+                    }
+                    if (yıgın.Count > 0) // Açık parantez varsa yapıdan çıkarır.
+                    {
+                        yıgın.Pop(); // Parantezi çıkart
+                    }
+                }
+                else // Operatör
+                {
+                    while (yıgın.Count > 0 && OncelikSırası(a) <= OncelikSırası(yıgın.Peek()))
+                    {
+                        cıktı += yıgın.Pop(); // Yığından eleman al
+                    }
+                    yıgın.Push(a); // Yığına operatör ekleme işlemi için
+                }
+            }
+            while (yıgın.Count > 0)
+            {
+                cıktı += yıgın.Pop(); 
+            }
+        
+            return cıktı;
+        }
+
+        private int OncelikSırası(char o)
+        {
+            switch (o)
+            {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                case '^':
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+
+        private string PrefixDonustur(string infix) 
+        {
+           
+            char[] karakterDizisi = infix.ToCharArray();
+            Array.Reverse(karakterDizisi);
+            string tersInfix = new string(karakterDizisi);
+
+           
+            tersInfix = tersInfix.Replace('(', 'X').Replace(')', '(').Replace('X', ')'); //Bu kısma dikkat et!!!
+
+            string postfix = PostfixDonusumu(tersInfix); // Ters infix'i postfix'e çevir
+
+            
+            char[] postfixDizisi = postfix.ToCharArray();
+            Array.Reverse(postfixDizisi);
+            
+            return new string(postfixDizisi); // Prefix ifadeyi döndür
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string infix = textBox1.Text;
+            string prefix = PrefixDonustur(infix);
+            label2.Text = "Prefix:  " + prefix;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string infix = textBox1.Text;
+            string postfix = PostfixDonusumu(infix);
+            label1.Text = "Postfix:  " + postfix;
+        }
+    }
+}
